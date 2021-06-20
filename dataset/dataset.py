6f5da58968436c49600f7553aac740fdc3256677
zengdun <928255708@qq.com>
2021-06-20 17:54:27 +0800

    update

---
+++ dataset/dataset.py
@@ -0,0 +1,17 @@
+from torch.utils.data import Dataset
+
+class BaseDataset(Dataset):
+    """ Basic Dataset Class """
+    def __init__(self, data, targets, transform=None):
+        self.data = data
+        self.targets= targets
+        self.transform = transform
+    
+    def __len__(self):
+        return len(self.targets)
+
+    def __getitem__(self, idx):
+        data, label = self.data[idx], self.targets[idx]
+        if self.transform:
+            data = self.transform(data)
+        return data, label
